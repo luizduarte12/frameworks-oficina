86b8d9ac3051574cd8ec9396885ad93ffcf80344
Felix-Vidal <joaofelixvidal@gmail.com>
2024-06-12 16:10:57 -0500

    Feat: Adicionado estudante

---
+++ webacademy/src/main/java/br/ufac/webacademy/models/Student.java
@@ -0,0 +1,26 @@
+package br.ufac.webacademy.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column(columnDefinition = "CHAR(11)", nullable = false, unique = true)
+    private String cpf;
+}
